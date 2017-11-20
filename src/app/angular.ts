31c8b437432ad32a5a614df5b08d778999cf758f
Pramudit <spramudit@gmail.com>
2017-11-21 01:09:44 +0530

    second commit

---
+++ src/app/angular.ts
@@ -0,0 +1,4 @@
+export class InsideModel {
+  id: number;
+  name: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ModalService } from './modal/modal.service';
+import { InsideModel } from '/angular.ts';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <button (click)='modalService.open(modalId)'>Open Modal</button>
+    <app-modal [modalTitle]="'Some title'" [blocking]='false' [modalId]='modalId'[(ngModel)]="Text.name" >
+      <div>{{Text.name}}</div>
+    </app-modal>
+  `
 })
+export class HeroesComponent implements OnInit {
+  Text: InsideModel = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+
 export class AppComponent {
-  title = 'app';
+modalId = 'hoplaModal';
+constructor(
+    public modalService: ModalService
+  ) {}
 }
